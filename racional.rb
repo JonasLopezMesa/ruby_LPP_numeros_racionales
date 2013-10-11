3ab21764ad4a10d7ff737409d66cddf564d63d61
Jonás López Mesa <77jonas77@gmail.com>
2013-10-11 12:24:37 +0100

    Añadido constructor de la clase y los tests correspondientes que prueban su funcionamiento

--- racional.rb
+++ racional.rb
@@ -3,7 +3,14 @@
 require "./gcd.rb"
 
 class Fraccion
+	#método para inicializar la clase
+	def initialize(numerador, denominador)
+		@num = numerador
+		@denom = denominador
+		true
+	end
 	def to_s
+
 		
 	end
 	def suma
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,11 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+	def test_constructor
+		assert Fraccion.new(4,6)
+		assert Fraccion.new(0,0)
+		assert Fraccion.new(-1,4)
+		assert Fraccion.new(7,200)
+		assert Fraccion.new(40,3)
+	end
 end
